6a89d02bf173c84d70a51c4885a7c6988f691576
Peter Donald <peter@realityforge.org>
2019-01-26 22:01:19 +1100

    Add required dagger module

--- src/main/java/react4j/drumloop/dagger/DrumLoopComponent.java
+++ src/main/java/react4j/drumloop/dagger/DrumLoopComponent.java
@@ -3,10 +3,11 @@ package react4j.drumloop.dagger;
 import dagger.Component;
 import javax.annotation.Nonnull;
 import javax.inject.Singleton;
+import react4j.drumloop.model.DrumMachineDaggerModule;
 import react4j.drumloop.views.DrumLoopViewDaggerComponentExtension;
 
 @Singleton
-@Component
+@Component( modules = DrumMachineDaggerModule.class )
 public interface DrumLoopComponent
   extends DrumLoopViewDaggerComponentExtension
 {
