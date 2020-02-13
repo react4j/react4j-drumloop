2aadb39e366ceaf006fc0c3b40aa742c6448c850
Peter Donald <peter@realityforge.org>
2020-02-14 08:35:22 +1100

    Update to the latest react4j/dagger integration

--- src/main/java/react4j/drumloop/dagger/DrumLoopComponent.java
+++ src/main/java/react4j/drumloop/dagger/DrumLoopComponent.java
@@ -4,27 +4,33 @@ import dagger.Component;
 import javax.annotation.Nonnull;
 import javax.inject.Singleton;
 import react4j.drumloop.model.DrumMachineDaggerModule;
-import react4j.drumloop.views.BpmInputDaggerComponentExtension;
-import react4j.drumloop.views.DrumMachineViewDaggerComponentExtension;
-import react4j.drumloop.views.IndicatorViewDaggerComponentExtension;
-import react4j.drumloop.views.PlayButtonDaggerComponentExtension;
+import react4j.drumloop.views.BpmInputFactory;
+import react4j.drumloop.views.DrumMachineViewFactory;
+import react4j.drumloop.views.IndicatorViewFactory;
+import react4j.drumloop.views.PlayButtonFactory;
 
+@SuppressWarnings( "UnusedReturnValue" )
 @Singleton
 @Component( modules = DrumMachineDaggerModule.class )
 public interface DrumLoopComponent
-  extends BpmInputDaggerComponentExtension,
-          DrumMachineViewDaggerComponentExtension,
-          IndicatorViewDaggerComponentExtension,
-          PlayButtonDaggerComponentExtension
 {
+  BpmInputFactory bpmInputFactory();
+
+  DrumMachineViewFactory drumMachineViewFactory();
+
+  IndicatorViewFactory indicatorViewFactory();
+
+  PlayButtonFactory playButtonFactory();
+
   @Nonnull
   static DrumLoopComponent create()
   {
     final DrumLoopComponent component = DaggerDrumLoopComponent.create();
-    component.bindBpmInput();
-    component.bindDrumMachineView();
-    component.bindIndicatorView();
-    component.bindPlayButton();
+    // These calls make sure the components are created and bound to the react factories
+    component.bpmInputFactory();
+    component.drumMachineViewFactory();
+    component.indicatorViewFactory();
+    component.playButtonFactory();
     return component;
   }
 }
